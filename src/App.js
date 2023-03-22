8b43c8d80637d5994b32db04ccae32d1a5d02a14
christoph.raddatz <hello@chrstphrddtz.com>
2023-03-22 10:57:58 +0100

    Add functionallity to Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Form } from './Component/Form/Form';
 
 import { useState } from 'react';
+import { uid } from 'uid';
 
 import './App.css';
 
 function App() {
 
-  const [activities, setActivities] = useState({})
+  const [activities, setActivities] = useState([])
 
   function handleAddActivity(newActivity) {
-    setActivities(...activities, newActivity)
+    setActivities([...activities, {id: uid(), ...newActivity}])
 
+    console.log(activities);
   }
 
 
--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,3 +1,4 @@
+import './Form.css'
 
 export function Form( { onAddActivity } ) {
 
